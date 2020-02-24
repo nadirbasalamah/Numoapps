574eaabefb99d0cdd9455d0ba5dcf6a02cb53b6d
Nadir <nadir008basalamah@gmail.com>
2020-02-24 10:47:05 +0700

    Initial commit

---
+++ app/src/main/java/nadirbasalamah/android/com/numoapps/MainActivity.kt
@@ -0,0 +1,12 @@
+package nadirbasalamah.android.com.numoapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
