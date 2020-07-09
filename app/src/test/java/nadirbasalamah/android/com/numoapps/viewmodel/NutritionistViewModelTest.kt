277e7502e8a51ad077b29d43d26d97b4e9a49707
Nadir <nadir008basalamah@gmail.com>
2020-07-09 08:20:26 +0700

    Unit tests finished

--- app/src/test/java/nadirbasalamah/android/com/numoapps/viewmodel/NutritionistViewModelTest.kt
+++ app/src/test/java/nadirbasalamah/android/com/numoapps/viewmodel/NutritionistViewModelTest.kt
@@ -2,17 +2,33 @@ package nadirbasalamah.android.com.numoapps.viewmodel
 
 import androidx.lifecycle.Observer
 import nadirbasalamah.android.com.numoapps.model.response.AntropometryResponse
+import nadirbasalamah.android.com.numoapps.util.NutritionistApiInterface
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class NutritionistViewModelTest {
-    private lateinit var nutritionistViewModel: NutritionistViewModel
+    private var mockWebServer = MockWebServer()
+    private lateinit var nutApiInterface: NutritionistApiInterface
 
     @Before
     fun before() {
-        nutritionistViewModel = NutritionistViewModel()
+        mockWebServer.start()
+        nutApiInterface = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("http://192.168.1.84:8080/klinikapps-api/public/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NutritionistApiInterface::class.java)
+    }
+
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
     }
 
     @Test
@@ -27,15 +43,49 @@ class NutritionistViewModelTest {
         data.put("visceral_fat","25")
         data.put("muscle","25")
         data.put("body_age","25")
-        val result = nutritionistViewModel.updateAntropometry(data)
-        assertEquals(true, result?.value?.status)
+
+        val result = nutApiInterface.postUpdateAntropometry(
+            data["id"]?.toInt(),
+            data["bb"]?.toFloat(),
+            data["tb"]?.toFloat(),
+            data["lila"]?.toFloat(),
+            data["bbi"]?.toFloat(),
+            data["fat"]?.toFloat(),
+            data["visceral_fat"]?.toFloat(),
+            data["muscle"]?.toFloat(),
+            data["body_age"]?.toFloat()
+        )?.execute()
+
+        val response = result?.body()
+        assertEquals(true, response?.status)
     }
 
     @Test
     fun testUpdateEmptyAntropometry() {
         val data: HashMap<String, String> = HashMap()
-        val result = nutritionistViewModel.updateAntropometry(data)
-        assertEquals(false, result?.value?.status)
+        data.put("id", "3")
+        data.put("tb","25")
+        data.put("lila","25")
+        data.put("bbi","25")
+        data.put("fat","25")
+        data.put("visceral_fat","25")
+        data.put("muscle","25")
+        data.put("body_age","25")
+
+        val result = nutApiInterface.postUpdateAntropometry(
+            data["id"]?.toInt(),
+            null,
+            data["tb"]?.toFloat(),
+            data["lila"]?.toFloat(),
+            data["bbi"]?.toFloat(),
+            data["fat"]?.toFloat(),
+            data["visceral_fat"]?.toFloat(),
+            data["muscle"]?.toFloat(),
+            data["body_age"]?.toFloat()
+        )?.execute()
+
+        val response = result?.body()
+        assertEquals(null, response?.status)
     }
 
     @Test
@@ -48,15 +98,43 @@ class NutritionistViewModelTest {
         data.put("lunch_time","14:00")
         data.put("dinner","Salad")
         data.put("dinner_time","18:30")
-        val result = nutritionistViewModel.addFoodMenu(data)
-        assertEquals(true, result?.value?.status)
+
+        val result = nutApiInterface.postAddFoodMenu(
+            data["id"]?.toInt(),
+            data["breakfast"],
+            data["breakfast_time"],
+            data["lunch"],
+            data["lunch_time"],
+            data["dinner"],
+            data["dinner_time"]
+        )?.execute()
+
+        val response = result?.body()
+        assertEquals(true, response?.status)
     }
 
     @Test
     fun testAddEmptyFoodMenu() {
         val data: HashMap<String, String> = HashMap()
-        val result = nutritionistViewModel.addFoodMenu(data)
-        assertEquals(false, result?.value?.status)
+        data.put("id","3")
+        data.put("breakfast","Nasi")
+        data.put("breakfast_time","07:00")
+        data.put("lunch_time","14:00")
+        data.put("dinner","Salad")
+        data.put("dinner_time","18:30")
+
+        val result = nutApiInterface.postAddFoodMenu(
+            data["id"]?.toInt(),
+            data["breakfast"],
+            data["breakfast_time"],
+            null,
+            data["lunch_time"],
+            data["dinner"],
+            data["dinner_time"]
+        )?.execute()
+
+        val response = result?.body()
+        assertEquals(null, response?.status)
     }
 
 }
--- app/src/test/java/nadirbasalamah/android/com/numoapps/viewmodel/UserViewModelTest.kt
+++ app/src/test/java/nadirbasalamah/android/com/numoapps/viewmodel/UserViewModelTest.kt
@@ -1,28 +1,50 @@
 package nadirbasalamah.android.com.numoapps.viewmodel
 
+import nadirbasalamah.android.com.numoapps.util.UserApiInterface
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.net.HttpURLConnection
 
 class UserViewModelTest {
-    private lateinit var userViewModel: UserViewModel
+    private var mockWebServer = MockWebServer()
+    private lateinit var userApiInterface: UserApiInterface
+
 
     @Before
-    fun before() {
-        userViewModel = UserViewModel()
+    fun setup() {
+        mockWebServer.start()
+        userApiInterface = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("http://192.168.1.84:8080/klinikapps-api/public/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserApiInterface::class.java)
     }
+
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
+
 
     @Test
     fun ViewNutRecord() {
-       val result = userViewModel.getNutRecordByUserId(3)
-       assertEquals(true, result?.value?.status)
+        val result = userApiInterface.getNutRecordById(6)?.execute()
+        val response = result?.body()
+        assertEquals(true, response?.status)
     }
 
     @Test
     fun ViewEmptyNutRecord() {
-        val result = userViewModel.getNutRecordByUserId(99)
-        assertEquals(false, result?.value?.status)
+        val result = userApiInterface.getNutRecordById(43)?.execute()
+        val response = result?.body()
+        assertEquals(null, response?.status)
     }
 
 }
